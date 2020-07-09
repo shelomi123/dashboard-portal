c959ccec7e4f8c03665b8953d761d4585b87d5fa
shelomi123 <shelomipriskila1995@gmail.com>
2020-07-10 00:27:43 +0530

    commit

--- src/content/adverts.js
+++ src/content/adverts.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import '../App.css';
 
+import {Link} from 'react-router-dom';
 
 function Adverts(){
     return (
         <div className="admin-content">
            <div className="adverts">
-               Adverts
+                <div className="row">
+                    <Link /*onClick={this.gologin}*/  className="adreqSend_btn">
+                        Advertisement Request
+                    </Link>
+                </div>
+               
+
+            
+               <div className="row">
+                    <button /*onClick={this.gologin}*/ type="button" className="adreqSend_btn" style={{ align:'right' }}>
+                        SEND
+                    </button>
+
+               </div>
+            
            </div>
+          
         </div>
+            
+        
     )
 }
 
