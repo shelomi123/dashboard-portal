a4f7b971defc94f6710879757f53a802b8d5f47b
shelomi123 <shelomipriskila1995@gmail.com>
2020-07-10 01:08:23 +0530

    bootstrap added

--- src/content/adverts.js
+++ src/content/adverts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import {Link} from 'react-router-dom';
 
 function Adverts(){
@@ -16,7 +16,7 @@ function Adverts(){
 
             
                <div className="row">
-                    <button /*onClick={this.gologin}*/ type="button" className="adreqSend_btn" style={{ align:'right' }}>
+                    <button /*onClick={this.gologin}*/ type="button" className="adreqSend_btn" style={{ align:'' }}>
                         SEND
                     </button>
 
