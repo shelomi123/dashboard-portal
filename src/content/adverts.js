2e6bb460a8a6999d87006d1a406fa030c1ca5278
shelomi123 <shelomipriskila1995@gmail.com>
2020-07-10 11:13:38 +0530

    sort added

--- src/content/adverts.js
+++ src/content/adverts.js
@@ -13,12 +13,33 @@ function Adverts(){
                     <Link to="/advertsRequest" className="advertsRequest" style={{ fontSize: '20px' }}>
                         click here
                     </Link>
+                </div>
+
+                <div class="container p-3 my-3 bg-light">
+                    <h5>Published advertisements</h5>
+
+                    <div class="form-group row">
+                        <label for="inputSort" class="col-sm-2 col-form-label">Sort By</label>
+                            <div class="col-sm-6">
+                                <select id="inputSort" class="form-control">
+                                    <option selected>Company</option>
+                                    <option>Job Role</option>
+                                </select>
+                            </div>
+                    </div>
+
+
+
+
+
+
+
                     
                 </div>
+                
 
             
            </div>
-          
         </div>
             
         
