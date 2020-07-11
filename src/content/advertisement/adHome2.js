8099329407131819509b67f73da824e42363ad7c
shelomi123 <shelomipriskila1995@gmail.com>
2020-07-11 12:42:10 +0530

    adHome2 modified

--- src/content/advertisement/adHome2.js
+++ src/content/advertisement/adHome2.js
@@ -1,32 +1,57 @@
 import React from 'react';
-import '../../App.css';
+import './ad.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 function Adverts(){
     return (
         <div className="admin-content">
-           <div className="container">
+             
 
+            <div class="container p-3 my-3 bg-light">
+                    <div class="form-group row">
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-info custom ">SUMMARY</button>
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-info custom">PENDING APPROVAL</button>
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-info custom">APPROVED</button>
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-info custom">DECLINED</button>
+                        </div>
+                    </div>
+            </div>
+
+            <div class="container p-3 my-3 bg-light">
+                <div class="form-group row">
+                    <label for="inputSort" class="col-sm-2 col-form-label"><h6>Filter By</h6> </label>
+                    <div class="col-md-2">
+                        <button type="button" class="btn btn-secondary btn-sm custom">Company</button>
+                    </div>
+                    <div class="col-md-2">
+                        <button type="button" class="btn btn-secondary btn-sm custom">Category</button>
+                    </div>
+                    <div class="col-md-2">
+                        <button type="button" class="btn btn-secondary btn-sm custom">None</button>
+                    </div>
+                </div>        
+
+                <div class="search-container">
+                    <form action="/action_page.php">
+                        <input class="col-md-11" type="text" placeholder="Search.." name="search"></input>
+                        <button type="submit">Submit</button>
+                    </form>
+  </div>
+                     fdffffffffff   
+            </div>
                 
-
-                <div class="container p-3 my-3 bg-light">
-                    <h3>Published advertisements</h3>
-
-                    
-
-                    
-
-
-
-
-
-                    
-                </div>
                 
 
             
-           </div>
+           
         </div>
             
         
--- src/views/adverts.js
+++ src/views/adverts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/navbar'
 import SideBar from '../components/sidebar'
-import Content from '../content/advertisement/adHome1';
+import Content from '../content/advertisement/adHome2';
 
 
 function Adverts(){
