41c70447da9a39929e37a50c0ed511e111eee3d7
Tharindu Basanayaka <tbasnayaka7@gmail.com>
2020-07-12 15:33:47 +0530

    set deadline ui
    without validations

--- src/views/adverts.js
+++ src/views/adverts.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from 'react'
 import Navbar from '../components/navbar'
 import SideBar from '../components/sidebar'
-import Content from '../content/adverts';
+import Content from '../content/adverts'
 
-
-function Adverts(){
-    return (
-        <div>
-        <Navbar/>
-        <SideBar/>
-        <Content/>
-        </div>
-    );
+function Adverts() {
+  return (
+    <div>
+      <Navbar />
+      <SideBar />
+      <Content />
+    </div>
+  )
 }
 
-export default Adverts;
+export default Adverts
