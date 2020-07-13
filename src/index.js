98d2632886ad4ea3fba3ba3c38236ddf475239b6
Tharindu Basanayaka <tbasnayaka7@gmail.com>
2020-07-13 11:53:08 +0530

    dependencies update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 import DashboardHome from './views/dashboardr-home'
 import Companies from './views/companies'
@@ -15,25 +13,25 @@ import StudentCvs from './views/studentCvs'
 import Emailing from './views/emailing'
 import Notifications from './views/notifications'
 import setDeadline from './content/advertisements2/setDeadline'
-import Users from './views/users';
+import Users from './views/users'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Switch>
-      <Route path="/dashboard" component={DashboardHome} />
-      <Route path="/companies" component={Companies} />
-      <Route path="/students" component={Students} />
-      <Route path="/adverts" component={Adverts} />
-      <Route path="/cvs" component={StudentCvs} />
-      <Route path="/emailing" component={Emailing} />
-      <Route path="/users" component={Users} />
-      <Route path="/notifications" component={Notifications} />
-  <Route path="/setDeadline" component={setDeadline} />
-      <Redirect from="/" to="/dashboard" />
-    </Switch>
-  </BrowserRouter>,
-
+      <Switch>
+        <Route path="/dashboard" component={DashboardHome} />
+        <Route path="/companies" component={Companies} />
+        <Route path="/students" component={Students} />
+        <Route path="/adverts" component={Adverts} />
+        <Route path="/cvs" component={StudentCvs} />
+        <Route path="/emailing" component={Emailing} />
+        <Route path="/users" component={Users} />
+        <Route path="/notifications" component={Notifications} />
+        <Route path="/setDeadline" component={setDeadline} />
+        <Redirect from="/" to="/dashboard" />
+      </Switch>
+    </BrowserRouter>
+    ,
   </React.StrictMode>,
   document.getElementById('root'),
 )
