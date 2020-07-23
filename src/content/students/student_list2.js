d1ad902b826a49f6f939b3ddb18b94671970b2a9
Amanda <thirunihp@gmail.com>
2020-07-23 18:21:40 +0530

    Added changes to student table

--- src/content/students/student_list2.js
+++ src/content/students/student_list2.js
@@ -3,13 +3,26 @@ import './students.css';
 import MUIDataTable from "mui-datatables";
 import {Button} from 'react-bootstrap';
 
-const columns = ["Student Name", "Registration No", "Index No", "Course" , "Email"]
+const columns = ["Student Name", "Registration No", "Index No", "Course" , "Email" , 
+                {
+                    name: "",
+                    options: {
+                    customBodyRender: (value, tableMeta, updateValue) => {
+                        return (
+                        <Button size="sm">
+                            {`View more`}
+                        </Button>
+                        );
+                    }
+                    }
+                }
+]
 
 const data = [
- ["Joe James", "2017cs005", "17000051", "CS" , "thirunihp@gmail.com"],
- ["John Walsh", "2017cs005", "17000051", "CS" , "thirunihp@gmail.com"],
- ["Bob Herm", "2017cs005", "17000051", "CS" , "thirunihp@gmail.com"],
- ["James Houston", "2017cs005", "17000051", "IS" , "thirunihp@gmail.com"]
+ ["Joe James", "2017cs005", "17000051", "CS" , "thirunihp@gmail.com" , ""],
+ ["John Walsh", "2017cs005", "17000051", "CS" , "thirunihp@gmail.com" , ""],
+ ["Bob Herm", "2017cs005", "17000051", "CS" , "thirunihp@gmail.com" , ""],
+ ["James Houston", "2017cs005", "17000051", "IS" , "thirunihp@gmail.com" , ""]
  
 ]
 
@@ -29,14 +42,9 @@ function student_list2(){
                     columns={columns} 
                     options={{options,
                         print : false,
+                        download: false,
                     }} 
                     />
-                    {/* <div className="p-2 " style={{ marginLeft: '48em' }}>
-                        <button
-                            className="btn btn-primary" style={{ width: '200px', marginRight: '2em' }}>Enroll
-                        </button>
-                        <button className="btn btn-danger" style={{ width: '200px' }}>Cancel</button>
-                    </div> */}
                     <div className="row">
                     <div className="col-7"></div>
                     <div className="btn_set col-5">    
