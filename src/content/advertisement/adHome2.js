116a44fce290fe20212ae1086dbb7b3df9c94f2b
shelomi123 <shelomipriskila1995@gmail.com>
2020-10-09 23:55:25 +0530

    commit

--- src/content/advertisement/adHome2.js
+++ src/content/advertisement/adHome2.js
@@ -15,21 +15,24 @@ function adHome2(){
         <div className="admin-content">
              
 
-            <div class="container p-3 my-3 bg-light">
-                    <div class="form-group row">
-                        <div class="col-md-3">
-                            <button type="button" class="btn btn-info custom "><Link style={{color:'white'}} to="/summary" >SUMMARY</Link></button>
-                        </div>
-                        <div class="col-md-3">
-                            <button type="button" class="btn btn-info custom"><Link style={{color:'white'}} to="/pending" >PENDING APPROVAL</Link></button>
-                        </div>
-                        <div class="col-md-3">
-                            <button type="button" class="btn btn-info custom" ><Link style={{color:'white'}} to="/approved" >APPROVED</Link></button>
-                        </div>
-                        <div class="col-md-3">
-                            <button type="button" class="btn btn-info custom"><Link style={{color:'white'}} to="/declined" >DECLINED</Link></button>
-                        </div>
+            <div class="container p-3 my-2 bg-light">
+                <div class="form-group row">
+                    <div class="col-md-2">
+                        <Link style={{ color: 'white' }} to="/adHome2" > <button type="button" class="btn btn-info custom ">HOME</button></Link>
                     </div>
+                    <div class="col-md-2">
+                        <Link style={{ color: 'white' }} to="/summary" > <button type="button" class="btn btn-info custom ">SUMMARY</button></Link>
+                    </div>
+                    <div class="col-md-2">
+                        <Link style={{ color: 'white' }} to="/pending" ><button type="button" class="btn btn-info custom">PENDING APPROVAL</button></Link>
+                    </div>
+                    <div class="col-md-2">
+                        <Link style={{ color: 'white' }} to="/approved" > <button type="button" class="btn btn-info custom" >APPROVED</button></Link>
+                    </div>
+                    <div class="col-md-2">
+                        <Link style={{ color: 'white' }} to="/declined" ><button type="button" class="btn btn-info custom">DECLINED</button></Link>
+                    </div>
+                </div>
             </div>
 
             <div class="container p-3 my-3 bg-light">
